3953f2ca9f580fdda1b2ebf2b26392c6c19a31e1
jieunlee <liz950827@gmail.com>
2022-04-19 14:15:11 +0900

    0419 js lecture

---
+++ 0419-js-lecture/test.js
@@ -0,0 +1,26 @@
+let lunch = 8000;
+let dinner = 10000;
+let gansick = 5500;
+
+// console.log(lunch+gansick+dinner+'원');
+
+// 쉼표로 구분
+// 함수는 함수를 선언(function declaration)하는 부분 = 함수를 만들어준다
+// 함수는 함수를 호출(function call)부 = 함수를 사용한다
+// 함수의 알맹이 = 알고리즘 => 소괄호 자리에 무언가만 들어갈 수 있음 = 함수의 입구
+// => 함수의 입구 재료가 되어줌 => 매개변수(parameter)
+function sickBe(a, b, c) {
+  // a+b+c;
+  // return '주현이는 비둘기를 좋아해';
+  // return을 하지 않으면 값을 꺼내오지 못함
+  // => 함수의 출구 = return(반환값): 돌려준다
+
+  // 빵공장에서 밀가루를 넣으면 빵이 만들어진다
+  // 밀가루가 없으면 참조 에러가 뜬다
+  let sohi = 'mansoos elder sister';
+  
+}
+
+// console.log(sickBe(lunch, gansick, dinner))
+console.log(sohi);
+
