9a8ecdf63941a98414febfd4ffad8e1b2f89f04f
jieunlee <liz950827@gmail.com>
2022-04-20 17:55:24 +0900

    0420 review document

---
+++ review/0420-example.js
@@ -0,0 +1,16 @@
+// console.log('hello');
+
+let one = 1;
+let two = 2;
+
+// typeof 연산자
+// console.log(one+two);
+// console.log(typeof (one+two));
+
+// if문, else문
+// if(one===two){
+//   console.log('같은 값입니다');
+// } else{
+//   console.log('다른 값입니다');
+// }
+
