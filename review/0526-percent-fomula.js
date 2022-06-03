48cd5931b9c94b5f19187c22cee154d7cb4af32b
이지은 <94373933+e-jieun@users.noreply.github.com>
2022-06-03 23:58:23 +0900

    percent function studying

---
+++ review/0526-percent-fomula.js
@@ -0,0 +1,14 @@
+// return percent of part value in total value
+function returnPercent(value,total){
+  let result = (total/value)*100;
+  return result;
+  console.log(result);
+}
+
+// return value by percent
+function returValue(total, percent){
+  let result = (total*percent)/100;
+  return result;
+  console.log(result);
+}
+
