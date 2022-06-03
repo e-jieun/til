e15277e268a9e4bccc2a620a976b78a40f5c7cb0
이지은 <94373933+e-jieun@users.noreply.github.com>
2022-06-04 01:39:25 +0900

    js studying

--- review/0526/script.js
+++ review/0526/script.js
@@ -1,17 +1,17 @@
-// return percent of part value in total value
+// 전체값에서 일부값 퍼센트 구하기
+// 1. (일부값/전체값)*100
 function returnPercent(value,total){
-  let result = (total/value)*100;
+  let result = (value/total)*100;
   return result;
 }
 console.log(returnPercent(20, 100));
 
 // return value by percent
-function returValue(total, percent){
+function returnValue(total, percent){
   let result = (total*percent)/100;
   return result;
-  console.log(result);
 }
-
+console.log(returnValue(100, 20));
 // number add n percent -> number*(1+percent/100);
 function numberAddPer(value, percent){
   let result = number*(1+percent/100);
