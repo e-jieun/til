92beabf818fcc6a08f66eff47a85f55e2ba42e8d
이지은 <94373933+e-jieun@users.noreply.github.com>
2022-07-29 14:48:32 +0900

    how to per, value return

--- 0526-percent-cal.js
+++ 0526-percent-cal.js
@@ -1,24 +1,28 @@
 // *1. 전체값에서 일부값 퍼센트는?
 // *일부값 / 전체값*100
 function returnPercent(total, value){
-  return value/total*100;
+  let result = value/total*100;
+  return result;
 }
 
 // *2. 전체값의 몇 퍼센트는 얼마?
 // *전체값 * 몇 퍼센트 / 100
 function returnPerValue(total, value){
-  return total*value/100;
+  let result = total*value/100;
+  return result;
 }
 
 
 // *3. 숫자를 몇 퍼센트 증가시키는 식?
 // *숫자 * (1+몇 퍼센트 / 100)
 function increasePer(currentValue, increasePer){
-  return currentValue*(1+increasePer/100);
+  let result = currentValue*(1+increasePer/100);
+  return result;
 }
 
 // *4. 숫자를 몇 퍼센트 감소시키는 식?
 // *숫자 * (1-몇 퍼센트 / 100)
 function decreasePer(currentValue, decreasePer){
-  return currentValue*(1-decreasePer/100);
+  let result = currentValue*(1-decreasePer/100);
+  return result;
 }
