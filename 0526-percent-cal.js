7e46823cd681a6f76ca2c2fb59d7d711b831b83f
jieunlee <liz950827@gmail.com>
2022-08-11 17:05:53 +0900

    기존 숫자의 몇 퍼센트만큼 증감하는 식을 알아둘 필요가 있음

--- 0526-percent-cal.js
+++ 0526-percent-cal.js
@@ -4,6 +4,7 @@ function returnPercent(total, value){
   let result = value/total*100;
   return result;
 }
+console.log(returnPercent(100, 10)+'%');
 
 // *2. 전체값의 몇 퍼센트는 얼마?
 // *전체값 * 몇 퍼센트 / 100
@@ -11,18 +12,20 @@ function returnPerValue(total, value){
   let result = total*value/100;
   return result;
 }
+console.log(returnPerValue(120, 20));
 
-
-// *3. 숫자를 몇 퍼센트 증가시키는 식?
-// *숫자 * (1+몇 퍼센트 / 100)
+// !3. 숫자를 몇 퍼센트 증가시키는 식?
+// !숫자 * (1+몇 퍼센트 / 100)
 function increasePer(currentValue, increasePer){
   let result = currentValue*(1+increasePer/100);
   return result;
 }
+console.log(increasePer(1, 100));
 
-// *4. 숫자를 몇 퍼센트 감소시키는 식?
-// *숫자 * (1-몇 퍼센트 / 100)
+// !4. 숫자를 몇 퍼센트 감소시키는 식?
+// !숫자 * (1-몇 퍼센트 / 100)
 function decreasePer(currentValue, decreasePer){
   let result = currentValue*(1-decreasePer/100);
   return result;
 }
+console.log(decreasePer(100, 10));
